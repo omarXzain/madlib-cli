40343c2cb6d539fa632034366e5deedd1c147f47
omarXzain <omaren2010@gmail.com>
2020-12-04 23:14:54 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,15 @@
+[PR](https://github.com/omarXzain/madlib-cli/pull/2)
+
+### Number and name of feature: Lab: 03 - Errors, Files, and Packaging
+
+- In this lab assignment you will be creating a command line application which takes advantage of Python’s built in capabilities for reading and writing files.
+
+- The function take a path for a text file containing the game text with {} around the required words and asks the user to input every word of them before showing the result to the user and it searchs for these words and replace them with the input
+
+### Estimate of time needed to complete: 5 hours
+
+### Start time: 2/12/2020 at 3:30 pm
+
+### Finish time: 3/12/2020 at 4:20 pm
+
+### Actual time needed to complete: 5 hours 
--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,5 +1,35 @@
 from madlib import __version__
+from madlib_cli.madlib import (parse,read_template,merge)
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_read():
+    with open('assets/your_answer.txt') as f:
+        contents = f.read()
+        actual = contents
+        expected = contents
+        assert actual == expected
+
+def test_parse():
+    with open('assets/template.txt') as x:
+        valList = x.read()
+        actual = valList
+        expected = valList
+        assert actual == expected
+
+
+def test_merge():
+    inputs = [ 'Madlib Game j is a really jj person. He likes  and jj on his free time. He once tripped on a j and started crying. j is a really j person.' ]
+    mergeStr = "tripped {}, really {} person {}?"
+    with open('assets/template.txt') as m:
+        mergeStr = m.read()
+        actual = mergeStr
+        expected = mergeStr
+    assert actual == expected
+    inputs = open('./assets/template.txt', 'r') 
+    content = inputs.read()	
+    inputs.close()
+    assert content == expected
+    return content
